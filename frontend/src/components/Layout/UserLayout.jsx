e58dda237a48f3145aacd608ed5a5d2e789d618d
Asm Shad (INT) <asm.shad@liz.com>
2025-02-19 09:08:16 +0600

    Footer Section Complete

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,15 +1,74 @@
-import React from 'react'
-import Header from '../Common/Header'
+import React from "react";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 const UserLayout = () => {
   return (
     <div>
-        {/* Header  */}
-        <Header></Header>
-        {/* Main Content  */}
-        {/* Footer  */}
+      {/* Header  */}
+      <Header></Header>
+      {/* Main Content  */}
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque odit,
+        molestias distinctio dolorem quia voluptatem ratione eius, laboriosam
+        nemo quasi odio officia ullam quas ex qui veritatis repudiandae?
+        Consectetur possimus perspiciatis cum doloribus molestiae neque
+        reiciendis. Placeat consequatur fugiat saepe enim accusamus consequuntur
+        eaque quidem nulla, id eum vero, vitae quas molestiae dignissimos magni
+        voluptates et quis aliquam nobis incidunt. Quidem, ullam corporis. Eius
+        non quae nostrum minima recusandae, dolorum blanditiis perferendis nisi
+        rem mollitia quod vel voluptates nobis labore! Eius obcaecati
+        necessitatibus fugiat nesciunt ab. Sapiente dolorem, repellendus
+        architecto ipsum ipsam cum corporis veniam non vero velit consectetur
+        possimus nam unde laborum nisi quos, distinctio blanditiis dolorum
+        tenetur molestias. Numquam aliquid eos vel laborum dolorum at ipsum sed
+        ullam molestias eveniet quisquam illo voluptas, quam, recusandae nihil
+        ad ut? Maiores atque eligendi, totam mollitia accusamus recusandae sed
+        minus quam temporibus. Fugiat quibusdam doloribus dolore sit, pariatur
+        dolor error facilis, repellendus numquam itaque officia praesentium quos
+        asperiores, rerum officiis laborum animi quidem sint. Nisi ipsa adipisci
+        expedita quae, quia minus aut molestias hic ullam veniam facere eaque
+        ratione dolores esse quam, tempora voluptatum unde laboriosam ex quis
+        accusantium maxime? Repellat, error? Nisi veritatis accusantium itaque
+        omnis dolorem commodi aliquid voluptas ut dolores doloribus, quisquam
+        impedit ipsa. Id dignissimos beatae quidem, culpa nam blanditiis ipsa
+        facilis eius atque dolorem exercitationem officia ducimus nisi maiores,
+        qui iusto harum quod eos eum esse error officiis delectus! Pariatur
+        nostrum quos laudantium in voluptatum, reiciendis accusamus laboriosam,
+        explicabo officiis nihil corporis amet, expedita suscipit quas deserunt
+        dolorum et. Minus, maxime esse? Quos, placeat perferendis. Aut eligendi
+        alias incidunt voluptatum assumenda quibusdam sunt accusantium pariatur
+        quod nam? Labore totam voluptates exercitationem consequuntur architecto
+        a esse illo iure? Obcaecati facilis culpa quae sit ex dolorem accusamus,
+        cum nihil pariatur voluptates quisquam similique veniam. Inventore rerum
+        odit neque aut laboriosam, excepturi voluptatum, at tempora commodi
+        placeat vero. Porro quas eum veritatis incidunt consequuntur illo
+        laborum quae aspernatur qui unde minima sequi itaque officiis voluptatum
+        sint nihil provident cum, ex quos dolore deleniti! Eveniet itaque rem,
+        voluptas commodi, quis hic quaerat inventore corrupti qui incidunt
+        obcaecati labore autem necessitatibus debitis corporis aut ab quod
+        veritatis cumque dicta molestiae repudiandae. Cumque obcaecati in iusto.
+        Fugiat obcaecati odit suscipit rem quibusdam est culpa soluta pariatur
+        dolores et! Nisi aut, sapiente nobis porro magnam exercitationem, eum,
+        saepe non distinctio tenetur provident atque earum laboriosam quas!
+        Libero mollitia dolor, sequi iste eius tempore ratione temporibus error
+        earum recusandae modi ullam hic consequatur excepturi quisquam aliquam
+        molestiae labore! Nobis pariatur earum dignissimos nostrum officiis
+        dolore nam nesciunt voluptate ipsa cumque corrupti excepturi sequi,
+        ipsum quaerat voluptates at eius expedita, perferendis id! Architecto
+        quidem officiis in, dignissimos ipsa enim debitis esse id eveniet maxime
+        tempore necessitatibus quis, nobis eligendi expedita eos aliquid
+        reprehenderit ullam veniam, voluptatum soluta tempora aperiam. Veritatis
+        sequi quas recusandae, eum ullam, officia obcaecati repellendus corporis
+        incidunt, voluptates adipisci dolorem iste? Molestiae dolorem delectus
+        aspernatur, dolore, reiciendis fugiat distinctio neque aperiam
+        perferendis sed laboriosam nostrum, sapiente reprehenderit cum voluptas
+        velit. Voluptate, illum?
+      </main>
+      {/* Footer  */}
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
