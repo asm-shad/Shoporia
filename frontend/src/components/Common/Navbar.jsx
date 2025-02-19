b3ad9ddb04141ee612669273f56de938999208da
Asm Shad (INT) <asm.shad@liz.com>
2025-02-19 08:16:52 +0600

    Mobile NavBar menu completed

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -7,10 +7,15 @@ import { RiMenuFold3Line, RiShoppingBag4Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
+import { GiCrossedAirFlows } from "react-icons/gi";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(true);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(true);
 
+  const toggleNavDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
 
   const toggleCartDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -70,7 +75,7 @@ const Navbar = () => {
             <SearchBar></SearchBar>
           </div>
 
-          <button className="md:hidden">
+          <button onClick={toggleNavDrawer} className="md:hidden">
             <RiMenuFold3Line className="h-7 w-7 text-gray-700"></RiMenuFold3Line>
           </button>
         </div>
@@ -79,6 +84,52 @@ const Navbar = () => {
         drawerOpen={drawerOpen}
         toggleCartDrawer={toggleCartDrawer}
       ></CartDrawer>
+      {/* Mobile Navigation */}
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        {/* Close Button */}
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavDrawer}>
+            <GiCrossedAirFlows className="h-6 w-6 text-gray-600 hover:text-gray-800"></GiCrossedAirFlows>
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="space-y-4">
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Men
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Women
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Bottom Wear
+            </Link>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
