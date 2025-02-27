0334319134ba00b2a0e81c9b88414902004daa60
Asm Shad (INT) <asm.shad@liz.com>
2025-02-27 22:06:04 +0600

    Best Seller Right section complete

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -58,6 +58,77 @@ const ProductDetails = () => {
             ))}
           </div>
           {/* Right Section */}
+          <div className="md:w-1/2 md:ml-10">
+            <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+              {selectedProduct.name}
+            </h1>
+            <p className="text-lg text-gray-600 mb-1 line-through">
+              {selectedProduct.originalPrice &&
+                `${selectedProduct.originalPrice}`}
+            </p>
+            <p className="text-xl text-gray-500 mb-2">
+              $ {selectedProduct.price}
+            </p>
+            <p className="text-gray-600 mb-4">{selectedProduct.description}</p>
+            <div className="mb-4">
+              <p className="text-gray-700">Color: </p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.colors.map((color) => (
+                  <button
+                    key={color}
+                    className="w-8 h-8 rounded-full border border-gray-200"
+                    style={{
+                      backgroundColor: color.toLocaleLowerCase(),
+                      filter: "brightness(0.5)",
+                    }}
+                  ></button>
+                ))}
+              </div>
+            </div>
+            <div className="mb-4">
+              <p className="text-gray-700">Size: </p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.sizes.map((size) => (
+                  <button
+                    key={size}
+                    className="px-4 py-2 rounded border border-gray-200"
+                  >
+                    {size}
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-700">Quantity: </p>
+              <div className="flex items-center space-x-4 mt-2">
+                <button className="px-2 py-1 bg-gray-200 rounded text-lg">
+                  -
+                </button>
+                <span className="text-lg">1</span>
+                <button className="px-2 py-1 bg-gray-200 rounded text-lg">
+                  +
+                </button>
+              </div>
+            </div>
+            <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
+              Add To Cart
+            </button>
+            <div className="mt-10 text-gray-700">
+              <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
+              <table className="w-full text-left text-sm text-gray-600">
+                <tbody>
+                  <tr>
+                    <td className="py-1">Brand</td>
+                    <td className="py-1">{selectedProduct.brand}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Material</td>
+                    <td className="py-1">{selectedProduct.material}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
