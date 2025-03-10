78de3c89fee2944a19ba46122628f86829f6e320
Asm Shad (INT) <asm.shad@liz.com>
2025-03-10 21:37:26 +0600

    Backened initiated and MongoDB connected

---
+++ backened/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express(); // Initialise express app
+app.use(express.json()); // Json data
+app.use(cors()); // To communicate with react server enable cors origie req
+
+dotenv.config();
+
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Shoporia API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// 5yGj53ltJc3x5ddz
