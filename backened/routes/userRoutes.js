48055a53297588e14ed17823c03524510e8ef5a9
Asm Shad (INT) <asm.shad@liz.com>
2025-03-11 09:43:49 +0600

    Post request successfully send, mongodb getting data and saving it

--- backened/routes/userRoutes.js
+++ backened/routes/userRoutes.js
@@ -13,7 +13,20 @@ router.post("/register", async (req, res) => {
 
   try {
     // Registration Logic
-    res.send({ name, email, password });
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
--- backened/server.js
+++ backened/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
+console.log("User routes loaded ✅");
 const app = express(); // Initialise express app
 app.use(express.json()); // Json data
 app.use(cors()); // To communicate with react server enable cors origin req
@@ -18,6 +19,9 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Welcome to Shoporia API!");
 });
+
+// API Routes (This will prepand .api/users in all our user routes)
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
