669dccd7a82c9249d11fce5038453a29bcb5bfa1
Asm Shad (INT) <asm.shad@liz.com>
2025-03-12 20:17:22 +0600

    Cart model and cart route created

--- backened/seeder.js
+++ backened/seeder.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const User = require("./models/User");
 const Product = require("./models/Product");
 const products = require("./data/products");
+const Cart = require("./models/Cart");
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const seedData = async () => {
     // Clear existing data
     await Product.deleteMany();
     await User.deleteMany();
+    await Cart.deleteMany();
 
     // Create a default admin User
     const createdUser = await User.create({
