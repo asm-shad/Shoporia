dc69f4e0da3f7de896594dc10b72ff2aa4461ed4
Asm Shad (INT) <asm.shad@liz.com>
2025-03-12 07:58:22 +0600

    Product Model created

--- backened/middleware/authMiddleware.js
+++ backened/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 // Middleware to protect routes
-const protect = async (req, resizeBy, next) => {
+const protect = async (req, res, next) => {
   let token;
 
   if (
@@ -12,12 +12,13 @@ const protect = async (req, resizeBy, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("Decoded Payload:", decoded);
 
       req.user = await User.findById(decoded.user.id).select("-password");
       next();
     } catch (error) {
       console.error("Token Verification Failed", error);
-      resizeBy.status(401).json({ message: "Not Authorized, token failed" });
+      res.status(401).json({ message: "Not Authorized, token failed" });
     }
   } else {
     res.status(401).json({ message: "Not authorized, No token provided" });
