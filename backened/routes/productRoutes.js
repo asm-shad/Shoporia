07cec6d4ef8d58a2e2d37764f2f2f7ada4a670e6
Asm Shad (INT) <asm.shad@liz.com>
2025-03-12 10:12:25 +0600

    dELETE Product route created

--- backened/routes/productRoutes.js
+++ backened/routes/productRoutes.js
@@ -129,4 +129,25 @@ router.put("/:id", protect, admin, async (req, res) => {
   }
 });
 
+// @route DELETE /api/products/:id
+// @desc Delete an product ID
+// @access Private/Admin
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    // Find the product by Id
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      // Remove the product from DB
+      await product.deleteOne();
+      res.json({ message: "Product Removed" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
