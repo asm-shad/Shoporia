a85aa48f475ba87594b1562fcb70951f89590f24
Asm Shad (INT) <asm.shad@liz.com>
2025-03-12 22:41:41 +0600

    Cart delete request created

--- backened/routes/cartRoutes.js
+++ backened/routes/cartRoutes.js
@@ -92,7 +92,7 @@ router.put("/", async (req, res) => {
   const { productId, quantity, size, color, guestId, userId } = req.body;
 
   try {
-    let cart = await getCart(userId, guesstId);
+    let cart = await getCart(userId, guestId);
     if (!cart) return res.status(404).json({ message: "Cart not found" });
 
     const productIndex = cart.products.findIndex(
@@ -109,6 +109,41 @@ router.put("/", async (req, res) => {
       } else {
         cart.products.splice(productIndex, 1); // Remove product if quantity is 0
       }
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route DELETE /api/cart
+// @desC Remove a product from the cart
+// @access Public
+router.delete("/", async (req, res) => {
+  const { productId, size, color, guestId, userId } = req.body;
+  try {
+    let cart = await getCart(userId, guestId);
+
+    if (!cart) return res.status(404).json({ message: "cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color
+    );
+
+    if (productIndex > -1) {
+      cart.products.splice(productIndex, 1);
+
       cart.totalPrice = cart.products.reduce(
         (acc, item) => acc + item.price * item.quantity,
         0
