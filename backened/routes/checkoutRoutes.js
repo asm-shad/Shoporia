9f04728f52446f6469c25574dc1df550533a8ea9
Asm Shad (INT) <asm.shad@liz.com>
2025-03-13 15:20:28 +0600

    Finalize chrckout and convert to an order after payment cinfirmation POST method created

--- backened/routes/checkoutRoutes.js
+++ backened/routes/checkoutRoutes.js
@@ -68,4 +68,48 @@ router.put("/:id/pay", protect, async (req, res) => {
   }
 });
 
+// @route POST /api/checkout/:id/finalize
+// @desc Finalize chrckout and convert to an order after payment cinfirmation
+// @access Private
+router.post("/:id/finalize", protect, async (req, res) => {
+  try {
+    const checkout = await Checkout.findById(req.params.id);
 
+    if (!checkout) {
+      return res.status(400).json({ message: "Checout not found." });
+    }
+
+    if (checkout.isPaid && !checkout.isFinalized) {
+      // Create the final order based on the checkout details
+      const finalOrder = await Order.create({
+        user: checkout.user,
+        orderItems: checkout.orderItems,
+        shippingAddress: checkout.shippingAddress,
+        paymentMethod: checkout.paymentMethod,
+        totalPrice: checkout.totalPrice,
+        isPaid: true,
+        paidAt: checkout.paidAt,
+        isDelivered: false,
+        paymentStatus: "paid",
+        paymentDetails: checkout.paymentDetails,
+      });
+
+      // Mark the checkout as finalized
+      checkout.isFinalized = true;
+      checkout.finalizedAt = Date.now();
+      await checkout.save();
+      // Delete the cart associated with the user
+      await Cart.findOneAndDelete({ user: checkout.user });
+      res.status(201).json(finalOrder);
+    } else if (checkout.isFinalized) {
+      res.status(400).json({ message: "Checkout already finalized." });
+    } else {
+      res.status(400).json({ message: "Checkout is not paid." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
