2b9f216f74bcdc7c84c624b0cf05f57f11629734
Asm Shad (INT) <asm.shad@liz.com>
2025-03-17 20:57:20 +0600

    OrderRoutes Completed and Checked

--- backened/routes/orderRoutes.js
+++ backened/routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Order = require("../models/Order");
 const { protect } = require("../middleware/authMiddleware");
 
-const route = express.Router();
+const router = express.Router();
 
 // @route GET /api/orders/my-orders
 // @desc Get logged-in users orders
@@ -10,7 +10,7 @@ const route = express.Router();
 router.get("/my-orders", protect, async (req, res) => {
   try {
     // Find orders for the authenticated user
-    const orders = await order.find({ user: req.user._id }).sort({
+    const orders = await Order.find({ user: req.user._id }).sort({
       createdAt: -1,
     }); //sort by most recent orders
     res.json(orders);
@@ -24,21 +24,19 @@ router.get("/my-orders", protect, async (req, res) => {
 // @desc Get order details by id
 // @access Private
 router.get("/:id", protect, async (req, res) => {
-  {
-    try {
-      const order = await Order.findById(req.params.id).populate(
-        "user",
-        "name email"
-      );
-      if (!order) {
-        return res.status(404).json({ message: "Order not found" });
-      }
-      // Return the full order details
-      res.json(order);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server Error" });
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
     }
+    // Return the full order details
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
--- backened/server.js
+++ backened/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 console.log("User routes loaded ✅");
 const app = express(); // Initialise express app
 app.use(express.json()); // Json data
@@ -25,9 +26,10 @@ app.get("/", (req, res) => {
 
 // API Routes (This will prepand .api/users in all our user routes)
 app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);  
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
