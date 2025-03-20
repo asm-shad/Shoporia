01767fa68d3ddf2a019abe04d1da376b4af35676
Asm Shad (INT) <asm.shad@liz.com>
2025-03-20 11:21:12 +0600

    Admin PUT and DELETE route created

--- backened/routes/adminRoutes.js
+++ backened/routes/adminRoutes.js
@@ -18,7 +18,7 @@ router.get("/", protect, admin, async (req, res) => {
   }
 });
 
-// @route GET /api/ADMIN/USERS
+// @route POST /api/admin/users
 // @desc Get all users (Admin Only)
 // @access Private/Admin
 router.post("/", protect, admin, async (req, res) => {
@@ -42,5 +42,49 @@ router.post("/", protect, admin, async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+// @route PULL /api/admin/users/:id
+// @desc Update user info (admin only) - Name, email and role
+// @access Private/Admin
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.role = req.body.role || user.role;
+    }
+    const updatedUser = await user.save();
+    res.json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route DELETE /api/admin/users/:id
+// @desc Delete a user
+// @access Private/Admin
+router.delete("/:id", protect, admin, async (req, res) => {
+    try {
+      // Find the user by ID
+      const user = await User.findById(req.params.id);
+      
+      // Check if the user exists
+      if (user) {
+        // Delete the user if found
+        await user.deleteOne();
+        res.json({ message: "User deleted successfully" });
+      } else {
+        // If the user is not found, send a 404 response
+        res.status(404).json({ message: "User not found" });
+      }
+    } catch (error) {
+      // Log and handle any server errors
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+  
 
 module.exports = router;
