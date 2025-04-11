875eb3542eb9636b7f64e7e753467942e2f7f91e
Asm Shad (INT) <asm.shad@liz.com>
2025-04-11 10:09:18 +0600

    CartContents Updated

--- frontend/src/components/Cart/CartContents.jsx
+++ frontend/src/components/Cart/CartContents.jsx
@@ -1,40 +1,37 @@
-import React from "react";
+import React, { useId } from "react";
 import { RiDeleteBin3Line } from "react-icons/ri";
+import {
+  removeFromCart,
+  updateCartItemQuantity,
+} from "../../redux/slices/cartSlice";
 
-const CartContents = () => {
-  const cartProducts = [
-    {
-      productId: 1,
-      name: "T-shirt",
-      size: "M",
-      color: "Red",
-      quantity: 1,
-      price: 15,
-      image: "https://picsum.photos/200?random=1",
-    },
-    {
-      productId: 2,
-      name: "Jeans",
-      size: "L",
-      color: "Blue",
-      quantity: 3,
-      price: 20,
-      image: "https://picsum.photos/200?random=2",
-    },
-    {
-      productId: 3,
-      name: "Pant",
-      size: "M",
-      color: "Black",
-      quantity: 1,
-      price: 25,
-      image: "https://picsum.photos/200?random=3",
-    },
-  ];
+const CartContents = ({ cart, userId, guestId }) => {
+  const dispatch = useDispatch();
+
+  // Handle adding or substracting from the cart
+  const handleAddToCart = (productId, delta, quantity, size, color) => {
+    const newQuantity = quantity + delta;
+    if (newQuantity >= 1) {
+      dispatch(
+        updateCartItemQuantity({
+          productId,
+          quantity: newQuantity,
+          guestId,
+          userId,
+          size,
+          color,
+        })
+      );
+    }
+  };
+
+  const handleRemoveFromCart = (productId, size, color) => {
+    dispatch(removeFromCart({ productId, guestId, userId, size, color }));
+  };
 
   return (
     <div>
-      {cartProducts.map((product, idx) => (
+      {cart.products.map((product, idx) => (
         <div
           key={idx}
           className="flex items-start justify-between py-4 border-b border-gray-300"
@@ -51,11 +48,33 @@ const CartContents = () => {
                 size: {product.size} | color: {product.color}
               </p>
               <div className="flex items-center mt-2">
-                <button className="border border-gray-300 rounded-md px-2 py-1 text-xl font-medium">
+                <button
+                  onClick={() =>
+                    handleAddToCart(
+                      product.productId,
+                      -1,
+                      product.quantity,
+                      product.size,
+                      product.color
+                    )
+                  }
+                  className="border border-gray-300 rounded-md px-2 py-1 text-xl font-medium"
+                >
                   -
                 </button>
                 <span className="mx-4">{product.quantity}</span>
-                <button className="border border-gray-300 rounded-md px-2 py-1 text-xl font-medium">
+                <button
+                  onClick={() =>
+                    handleAddToCart(
+                      product.productId,
+                      1,
+                      product.quantity,
+                      product.size,
+                      product.color
+                    )
+                  }
+                  className="border border-gray-300 rounded-md px-2 py-1 text-xl font-medium"
+                >
                   +
                 </button>
               </div>
@@ -63,7 +82,15 @@ const CartContents = () => {
           </div>
           <div>
             <p>$ {product.price}</p>
-            <button>
+            <button
+              onClick={() =>
+                handleRemoveFromCart(
+                  product.productId,
+                  product.size,
+                  product.color
+                )
+              }
+            >
               <RiDeleteBin3Line className="h-6 w-6 mt-2 text-red-600"></RiDeleteBin3Line>
             </button>
           </div>
