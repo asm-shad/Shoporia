acea28500cfa289aeb0ab867190cec77c175ccb2
Asm Shad (INT) <asm.shad@liz.com>
2025-04-29 09:02:59 +0600

    AdminHomePage Updated for Dynamic value

--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -1,73 +1,50 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
-const orders = [
-  {
-    _id: 123123,
-    user: {
-      name: "ASM Shad",
-    },
-    totalPrice: 110,
-    status: "Processing",
-  },
-  {
-    _id: 123123,
-    user: {
-      name: "ASM Shad",
-    },
-    totalPrice: 110,
-    status: "Processing",
-  },
-  {
-    _id: 123123,
-    user: {
-      name: "ASM Shad",
-    },
-    totalPrice: 110,
-    status: "Processing",
-  },
-  {
-    _id: 123123,
-    user: {
-      name: "ASM Shad",
-    },
-    totalPrice: 110,
-    status: "Processing",
-  },
-  {
-    _id: 123123,
-    user: {
-      name: "ASM Shad",
-    },
-    totalPrice: 110,
-    status: "Processing",
-  },
-];
+import { fetchAdminProducts } from "../redux/slices/adminProductSlice";
+import { fetchAllOrders } from "../redux/slices/adminOrderSlice";
+import { useEffect } from "react";
 
 const AdminHomePage = () => {
+  const dispatch = useDispatch();
+  const {products, loading: productsLoading, error: productsError} = useSelector((state) => state.adminProducts);
+  const {orders, totalOrders, totalSales, loading: ordersLoading, error: ordersError} = useSelector((state) => state.adminOrders);
+
+  useEffect(() => {
+    dispatch(fetchAdminProducts());
+    dispatch(fetchAllOrders())
+;  }, [dispatch]);
+
   return (
     <div className="max-w-7xl mx-auto p-6">
       <h1 className="text3xl font-bold mb-6">Admin Dashboard</h1>
+      {productsLoading || ordersLoading ? (
+        <p>Loading...</p>
+      ): productsError ? (
+        <p className="text-red-500">Error fetching products: {productsError}</p>
+      ) : ordersError ? (
+        <p className="text-red-500">Error fetching products: {ordersError}</p>
+      ) : (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="p-4  shadow-md rounded-lg">
           <h2 className="text-xl font-semibold">Revenue</h2>
-          <p className="text-2xl">$10000</p>
+          <p className="text-2xl">${totalSales.toFixed(2)}</p>
         </div>
         <div className="p-4  shadow-md rounded-lg">
           <h2 className="text-xl font-semibold">Total Orders</h2>
-          <p className="text-2xl">200</p>
+          <p className="text-2xl">{totalOrders}</p>
           <Link to="/admin/orders" className="text-blue-500 hover:underline">
             Manage Orders
           </Link>
         </div>
         <div className="p-4  shadow-md rounded-lg">
           <h2 className="text-xl font-semibold">Total Products</h2>
-          <p className="text-2xl">100</p>
+          <p className="text-2xl">{products.length}</p>
           <Link to="/admin/products" className="text-blue-500 hover:underline">
             Manage Products
           </Link>
         </div>
       </div>
+      )};
       <div className="mt-6">
         <h2 className="text-2xl font-bold mb-4">Recent Orders</h2>
         <div className="overflow-x-auto">
@@ -87,7 +64,7 @@ const AdminHomePage = () => {
                   >
                     <td className="p-4">{order._id}</td>
                     <td className="p-4">{order.user.name}</td>
-                    <td className="p-4">{order.totalPrice}</td>
+                    <td className="p-4">{order.totalPrice.toFixed(2)}</td>
                     <td className="p-4">{order.status}</td>
                   </tr>
                 ))
