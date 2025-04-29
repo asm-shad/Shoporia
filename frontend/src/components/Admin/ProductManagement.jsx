b0eb4312fcac22908bf0d25a86670dbbeaa33e6f
Asm Shad (INT) <asm.shad@liz.com>
2025-04-29 12:15:48 +0600

    Edit Product File page completed functionality

--- frontend/src/components/Admin/ProductManagement.jsx
+++ frontend/src/components/Admin/ProductManagement.jsx
@@ -1,22 +1,29 @@
-import React from "react";
-import { GiPriceTag } from "react-icons/gi";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { deleteProduct, fetchAdminProducts } from "../../redux/slices/adminProductSlice";
 
 const ProductManagement = () => {
-  const products = [
-    {
-      _id: 123123,
-      name: "Shirt",
-      price: 300,
-      sku: "123123132",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector((state) => state.adminProducts);
+
+  useEffect(() => {
+    dispatch(fetchAdminProducts());
+  }, [dispatch]);
 
   const handleDelete = (id) => {
     if (window.confirm("Are you sure you want to delete the user?")) {
-      console.log("deleting product with ID", id);
+      dispatch(deleteProduct(id));
     }
   };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <div className="max-w-7xl mx-auto p-6">
