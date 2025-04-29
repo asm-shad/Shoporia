35b9fc29dd1b7d0d9938b1a14350a3d174d4bde3
Asm Shad (INT) <asm.shad@liz.com>
2025-04-29 21:34:48 +0600

    Final touch on login and Register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const { user, guestId } = useSelector((state) => state.auth);
+  const { user, guestId, loading } = useSelector((state) => state.auth);
   const { cart } = useSelector((state) => state.cart);
 
   // Get redirect parameter and check if it's checkout or something
@@ -76,7 +76,7 @@ const Login = () => {
             type="submit"
             className="bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition w-full cursor-pointer"
           >
-            Sign In
+            {loading ? "Loading..." : "Sign In"}
           </button>
           <p className="mt-6 text-center text-sm">
             Don&apos;t have an account?
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const { user, guestId } = useSelector((state) => state.auth);
+  const { user, guestId, loading } = useSelector((state) => state.auth);
   const { cart } = useSelector((state) => state.cart);
 
   // Get redirect parameter and check if it's checkout or something
@@ -87,7 +87,7 @@ const Register = () => {
             type="submit"
             className="bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition w-full cursor-pointer"
           >
-            Sign Up
+            {loading ? "Loading..." : "Sign Up"}
           </button>
           <p className="mt-6 text-center text-sm">
             Already have an account?{" "}
